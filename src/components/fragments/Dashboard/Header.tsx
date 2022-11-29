89c6cbb2353fa3f774094570639aab32b1ab6d97
Vision <visiondaniels32@gmail.com>
2022-11-29 22:10:49 +0000

    Init Dashboard Page

---
+++ src/components/fragments/Dashboard/Header.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+import { BiLogOut } from 'react-icons/bi';
+import Button from '../Common/Button';
+
+const DashboardHeader = () => {
+  return (
+    <div className="flex justify-between p-6 shadow">
+      <Image src="/assets/images/logo.png" alt="Restaurant Logo" width={200} height={46} className="ml-auto" />
+      <Button variant="danger" prefix={<BiLogOut />} className="ml-auto">
+        Logout
+      </Button>
+    </div>
+  );
+};
+
+export default DashboardHeader;
