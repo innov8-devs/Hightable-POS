e1a5680b0992373a8f8f618ed47544a425f395db
Vision <visiondaniels32@gmail.com>
2022-11-29 23:17:42 +0000

    Grid menu setup

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,3 +12,38 @@ export const buttonVariantStyle = {
   danger: 'bg-danger p-2 text-white text-semibold rounded-md',
   unstyled: '',
 };
+
+export const dashboardOptions = [
+  {
+    name: 'Menu',
+    icon: '/assets/svgs/menu.svg',
+  },
+  {
+    name: 'Kitchen',
+    icon: '/assets/svgs/kitchen.svg',
+  },
+  {
+    name: 'Bar',
+    icon: '/assets/svgs/drink.svg',
+  },
+  {
+    name: 'Sync Data',
+    icon: '/assets/svgs/sync.svg',
+  },
+  {
+    name: 'Void Tickets',
+    icon: '/assets/svgs/void.svg',
+  },
+  {
+    name: 'Financial Report',
+    icon: '/assets/svgs/report.svg',
+  },
+  {
+    name: 'Clock in/out',
+    icon: '/assets/svgs/clockIn.svg',
+  },
+  {
+    name: 'User Settings',
+    icon: '/assets/svgs/settings.svg',
+  },
+];
